01e403a7debe0fc204d3151d6c62bb1177d72b22
bjpatterson <brandonpatterson99@yahoo.com>
2016-08-24 20:07:23 -0400

    Add moxunit tests
    Do this as a step toward CI

---
+++ tests/test_exp_approx.m
@@ -0,0 +1,14 @@
+function test_suite = test_exp_approx()
+    initTestSuite;
+
+function test_negative_degree
+    assertEqual(exp_approx(1,-1), 0)
+
+function test_degree_zero
+    assertEqual(exp_approx(1,0),1)
+
+function test_accuracy_of_e
+    assertElementsAlmostEqual(exp_approx(1,25), 2.71828182845905)
+
+function test_accuracy_of_e_squared
+    assertElementsAlmostEqual(exp_approx(2.3,30), 9.97418245481472)
