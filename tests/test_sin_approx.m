bb502e05b1aaceea8b39a4e87c0cc77ff0678005
bjpatterson <bjpatterson@bsu.edu>
2016-08-26 19:07:32 -0400

    Add tests for the new trig functions
    Do this as a best practice to verify proper functionality

---
+++ tests/test_sin_approx.m
@@ -0,0 +1,14 @@
+function test_suite = test_sin_approx()
+    initTestSuite;
+
+function test_negative_degree
+    assertEqual(sin_approx(1,-1), 0)
+
+function test_degree_zero
+    assertEqual(sin_approx(1,0),0)
+    
+function test_low_order_correctness
+    assertEqual(sin_approx(1,3),5/6)
+
+function test_accuracy
+    assertElementsAlmostEqual(sin_approx(1,25), 0.841470984807897)
---
+++ tests/test_tan_approx.m
@@ -0,0 +1,17 @@
+function test_suite = test_tan_approx()
+    initTestSuite;
+
+function test_negative_degree_crash
+    assertExceptionThrown(tan_approx(1,-1))
+
+function test_degree_zero
+    assertEqual(tan_approx(1,0),0)
+    
+function test_low_order_correctness
+    assertEqual(tan_approx(1,3),5/3)
+
+function test_accuracy
+    assertElementsAlmostEqual(
+    tan_approx(1,25),
+    0.841470984807897/0.54030230586814
+)
