9a8dd6c909a9e1455c9fe937c562fa91deb25b00
bjpatterson <bjpatterson@bsu.edu>
2016-08-31 16:42:13 -0400

    add hardcoded f(x) and df(x)
    Do this to get started with root approximations (We know it's bad form)

---
+++ functions/df.m
@@ -0,0 +1,5 @@
+function [y] = df (x)
+  # hardcoded function (bad form)
+  # d/dx of y = x^2 - 3
+  y = 2*x;
+end
---
+++ functions/f.m
@@ -0,0 +1,5 @@
+function [y] = f(x)
+  # hardcoded function (bad form)
+  # y = x^2 - 3
+  y = x^2 - 3;
+end
