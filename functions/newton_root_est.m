2e837ba10a0fef746edfe20fadb9cd825f6b7ab1
bjpatterson <bjpatterson@bsu.edu>
2016-08-31 16:43:17 -0400

    Add an (empty) implementation of Newton's
    (For later work on another machine)

---
+++ functions/newton_root_est.m
@@ -0,0 +1,5 @@
+function [root_est] = newton_root_est (estimate, epsilon)
+    # Newton's method for solving f(x) = 0
+    # Stopping criteria is abs(estimate - previous_estimate) < epsilon
+    
+end
