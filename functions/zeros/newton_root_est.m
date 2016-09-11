086642875645a3a6a872bc2c6d79b1fa7f1cd26d
bjpatterson <bjpatterson@bsu.edu>
2016-09-11 18:15:16 -0400

    add estimate history to newton's & secant methods
    Do this so that the estimates can be used to determine root multiplicity

--- functions/zeros/newton_root_est.m
+++ functions/zeros/newton_root_est.m
@@ -1,4 +1,4 @@
-function [root_est] = newton_root_est (f, df, init_est, epsilon, max_iter)
+function [root_est, all_ests] = newton_root_est (f, df, init_est, epsilon, max_iter)
 # Implementation of Newton's method for solving f(x) = 0
 # Stopping criteria is abs(current_estimate - previous_estimate) < epsilon
 #
@@ -11,6 +11,7 @@ function [root_est] = newton_root_est (f, df, init_est, epsilon, max_iter)
 # Usage Examples:
 #     newton_root_est('x^2-3', '2*x', 1.5, .00001, 1000)
 #     newton_root_est(@(y) cos(y)-.2, @(y) -sin(y), 1, 10^-8, 1000)
+#     [estimate, previous_estimates] = newton_root_est (f, df, init_est, epsilon, max_iter)
     
     if(ischar(f))
         f = inline(f);
@@ -22,6 +23,7 @@ function [root_est] = newton_root_est (f, df, init_est, epsilon, max_iter)
     root_est = init_est;
     
     for iter = 1:max_iter
+        all_ests(iter) = root_est;
         error_est = -f(root_est)/df(root_est);
         root_est += error_est;
         if(abs(error_est) <= epsilon)
