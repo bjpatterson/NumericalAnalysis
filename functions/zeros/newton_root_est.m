b9d8fe917019ee9a5bdb78dc8a74461865731b41
bjpatterson <bjpatterson@bsu.edu>
2016-09-14 21:56:43 -0400

    update docstrings
    Do this to reflect new changes and aid in readability

--- functions/zeros/newton_root_est.m
+++ functions/zeros/newton_root_est.m
@@ -2,11 +2,17 @@ function [root_est, all_ests] = newton_root_est (f, df, init_est, epsilon, max_i
 # Implementation of Newton's method for solving f(x) = 0
 # Stopping criteria is abs(current_estimate - previous_estimate) < epsilon
 #
-# f is a function (as a string or an anonymous function)
-# df is the first derivative of f (as a string or an anonymous function)
-# init_est is an initial approximation to iterate from
-# epsilon is an approximate acceptable degree of error
-# max_iter is the maximum iterations before returning (in case of divergence)
+# PARAMS:
+#   f is a function (as a string or an anonymous function)
+#   df is the first derivative of f (as a string or an anonymous function)
+#   init_est is an initial approximation to iterate from
+#   epsilon is an approximate acceptable degree of error
+#   max_iter is the maximum iterations before returning (in case of divergence)
+#
+# RETURNS:
+#   root_est is the best estimate of a root to the given function f
+#   all_ests is the estimate history, where all_ests(n) is the nth estimate
+# 
 #
 # Usage Examples:
 #     newton_root_est('x^2-3', '2*x', 1.5, .00001, 1000)
--- functions/zeros/secant_root_est.m
+++ functions/zeros/secant_root_est.m
@@ -2,10 +2,17 @@ function [root_est, all_ests] = secant_root_est (f, init_est, epsilon, max_iter)
 # Implementation of the secant method for solving f(x) = 0
 # Stopping criteria is abs(current_estimate - previous_estimate) < epsilon
 #
-# f is a function (as a string or an anonymous function)
-# init_est is an initial approximation to iterate from
-# epsilon is an approximate acceptable degree of error
-# max_iter is the maximum iterations before returning (in case of divergence)
+#
+# PARAMS:
+#   f is a function (as a string or an anonymous function)
+#   init_est is an initial approximation to iterate from
+#   epsilon is an approximate acceptable degree of error
+#   max_iter is the maximum iterations before returning (in case of divergence)
+#
+# RETURNS:
+#   root_est is the best estimate of a root to the given function f
+#   all_ests is the estimate history, where all_ests(n) is the nth estimate
+#
 #
 # Usage Examples:
 #     secant_root_est('x^2-3', 1.5, .00001, 1000)
