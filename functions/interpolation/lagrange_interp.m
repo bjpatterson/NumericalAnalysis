2c2eba4649cf11a244e3943bf1ec311b18c4f976
bjpatterson <bjpatterson@bsu.edu>
2016-09-20 19:06:12 -0400

    rename vectors with capital letters
    Do this to make types more intuitive

--- functions/interpolation/lagrange_interp.m
+++ functions/interpolation/lagrange_interp.m
@@ -1,46 +1,46 @@
-function [y_interp] = lagrange_interp (x_point, y_point, x_interp)
+function [Y_interp] = lagrange_interp (X, Y, X_interp)
 #
 # Given a list of set points, interpolates y values for additional x points
 # using Lagrange polynomials
 #
 # INPUTS:
-#   x_point: a vector of x data points
-#   y_point: a vector of y data points corresponding to x_point
-#   x_interp: a vector of x values for interpolating based on x_point and y_point
+#   X: a vector of x data points
+#   Y: a vector of y data points corresponding to X
+#   X_interp: a vector of x values for interpolating based on X and Y
 #
 # OUTPUTS:
-#   y_interp: the interpolated y values
+#   Y_interp: the interpolated y values
 #
 
-    if(!isrow(x_point) || !isrow(y_point) || length(x_point)!=length(y_point))
-        display('ERROR: x_point and y_point must be row vectors of equal length')
-        y_interp = NaN;
-    elseif(!isrow(x_interp))
-        display('ERROR: x_point must be either a scalar or a row vector')
-        y_interp = NaN;
-    elseif(!length(x_point) == length(unique(x_point)))
-        display('ERROR: x_point cannot contain duplicate values!')
-        y_interp = NaN;
+    if(!isrow(X) || !isrow(Y) || length(X)!=length(Y))
+        display('ERROR: X and Y must be row vectors of equal length')
+        Y_interp = NaN;
+    elseif(!isrow(X_interp))
+        display('ERROR: X must be either a scalar or a row vector')
+        Y_interp = NaN;
+    elseif(!length(X) == length(unique(X)))
+        display('ERROR: X cannot contain duplicate values!')
+        Y_interp = NaN;
     else
-        if(min(x_interp) < min(x_point) || max(x_interp) > max(x_point))
+        if(min(X_interp) < min(X) || max(X_interp) > max(X))
             display('WARNING: interpolation points lie outside the observed range')
         end
         
-        n = length(x_point);
-        l_poly = ones(n,length(x_interp));
+        n = length(X);
+        l_poly = ones(n,length(X_interp));
         
         for i = 1:n
             for j = 1:n
                 if(i != j)
-                    l_poly(i,:) .*= (x_interp-x_point(j))/(x_point(i)-x_point(j));
+                    l_poly(i,:) .*= (X_interp-X(j))/(X(i)-X(j));
                 end
             end
         end
         
-        y_interp = zeros(1, length(x_interp));
+        Y_interp = zeros(1, length(X_interp));
         
         for i = 1:n
-            y_interp += y_point(i)*l_poly(i,:);
+            Y_interp += Y(i)*l_poly(i,:);
         end
         
     end
