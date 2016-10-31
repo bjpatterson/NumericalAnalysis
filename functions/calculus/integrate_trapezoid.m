682a37f92a2e8a17ee42035d37c6c7ad0ccf953a
bjpatterson <bjpatterson@bsu.edu>
2016-10-31 19:38:21 -0400

    add doctstrings and improve trapezoid rule
    good practice

--- functions/calculus/integrate_trapezoid.m
+++ functions/calculus/integrate_trapezoid.m
@@ -1,5 +1,16 @@
 function [est] = integrate_trapezoid (f, a, b, n=1000)
-
+# Estemates the integral of a function f over the interval (a,b) using n trapezoids
+#
+# IN:
+#   f: a univariate function (as a string or anonymous function)
+#   a, b: integration bounds
+#   n: the number of partitions to use (1000 by default)
+#
+# OUT:
+#   est: an estimate of the integral value
+#
+# usage:
+#   integrate_trapezoid(@(x) x^2, 0, 5, 200)
 
   if(b==a)
       est = 0,
@@ -16,8 +27,8 @@ function [est] = integrate_trapezoid (f, a, b, n=1000)
   
   h = (b-a)/n;
   X = a:h:b;
-  Y = f(X);
+  Y = arrayfun(f, X);
   
-  est = (2 * sum(Y) - Y(1) - Y(n+1)) / 2 / n;
+  est = h * (2 * sum(Y) - Y(1) - Y(n+1)) / 2;
   
 end
