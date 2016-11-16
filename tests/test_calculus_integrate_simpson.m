469c715412a062b557f671b454e33038d94a5c23
bjpatterson <bjpatterson@bsu.edu>
2016-11-15 22:12:56 -0500

    refine tests for simplicity
    because some of the code was superfluous

--- tests/test_calculus_integrate_simpson.m
+++ tests/test_calculus_integrate_simpson.m
@@ -2,10 +2,10 @@ function test_suite = test_calculus_integrate_simpson ()
     initTestSuite;
 
 function test_no_crash
-    y = integrate_simpson("sin(x)", 0, pi/2);
-    y = integrate_simpson(@(x)sin(x), 0, pi/2);
-    y = integrate_simpson("sin(x)", 0, pi/2, 100);
-    y = integrate_simpson(@(x)sin(x), 0, pi/2, 100);
+    integrate_simpson("sin(x)", 0, pi/2);
+    integrate_simpson(@(x)sin(x), 0, pi/2);
+    integrate_simpson("sin(x)", 0, pi/2, 100);
+    integrate_simpson(@(x)sin(x), 0, pi/2, 100);
     
 function test_correctness
     assertEqual(integrate_simpson(@(x)x^2, 0, 2), 8/3);
--- tests/test_calculus_integrate_trapezoid.m
+++ tests/test_calculus_integrate_trapezoid.m
@@ -2,10 +2,10 @@ function test_suite = test_calculus_integrate_trapezoid ()
     initTestSuite;
 
 function test_no_crash
-    y = integrate_trapezoid("sin(x)", 0, pi/2);
-    y = integrate_trapezoid(@(x)sin(x), 0, pi/2);
-    y = integrate_trapezoid("sin(x)", 0, pi/2, 100);
-    y = integrate_trapezoid(@(x)sin(x), 0, pi/2, 100);
+    integrate_trapezoid("sin(x)", 0, pi/2);
+    integrate_trapezoid(@(x)sin(x), 0, pi/2);
+    integrate_trapezoid("sin(x)", 0, pi/2, 100);
+    integrate_trapezoid(@(x)sin(x), 0, pi/2, 100);
     
 function test_correctness
     assertElementsAlmostEqual(integrate_trapezoid(@(x)x^2, 0, 2, 5000), 8/3);
