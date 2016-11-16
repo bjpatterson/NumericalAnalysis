a6324fe23bc2eeed4790b4d0b6c5fd0e30bb34c6
bjpatterson <bjpatterson@bsu.edu>
2016-11-15 22:15:12 -0500

    fix gaussian quadrature tests
    now that CI has been verified, stop crashing intentionally and use
    correct tests

--- tests/test_calculus_integrate_gauss.m
+++ tests/test_calculus_integrate_gauss.m
@@ -12,4 +12,3 @@ function test_no_crash
 function test_correctness
     assertElementsAlmostEqual(integrate_gauss(@(x)x^5, 0, 2,1,3), 32/3);
     assertElementsAlmostEqual(integrate_gauss('sin(x)',0,pi/2,1,5),1);
-    assert(0==1) # to ensure tests are being run by CI
